0f4261de60c98d9b26c9a3f9514ba6ffff7b45c2
dltmdgkr <shl907@naver.com>
2024-04-12 15:04:25 +0900

    feat: 페이지 새로고침 시 탭이 유지 되지 않음 => useSearchParams를 이용한 tab default value 적용

--- src/app/(AfterLogin)/community/create/page.tsx
+++ src/app/(AfterLogin)/community/create/page.tsx
@@ -1,13 +1,22 @@
+"use client";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ReviewPostForm from "./_component/ReviewPostForm";
 import StudyPostForm from "./_component/StudyPostForm";
+import { useSearchParams } from "next/navigation";
+import Link from "next/link";
 
 export default function Page() {
+  const tabParams = useSearchParams().get("tab") || undefined;
   return (
-    <Tabs defaultValue="reviews" className="w-full px-10 py-5">
+    <Tabs defaultValue={tabParams} className="w-full px-10 py-5">
       <TabsList>
-        <TabsTrigger value="reviews">면접 후기</TabsTrigger>
-        <TabsTrigger value="studies">스터디 모집</TabsTrigger>
+        <Link href="/community/create?tab=reviews">
+          <TabsTrigger value="reviews">면접 후기</TabsTrigger>
+        </Link>
+        <Link href="/community/create?tab=studies">
+          <TabsTrigger value="studies">스터디 모집</TabsTrigger>
+        </Link>
       </TabsList>
       <TabsContent value="reviews">
         <ReviewPostForm />
