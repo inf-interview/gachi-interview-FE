547d878cec8553a581d8555c2603cf3b508776c4
Lee-Young-Jae <ori_@kakao.com>
2024-04-25 16:25:02 +0900

    refactor: QuestionListHeader의 모든 질문을 선택하는 기능을 현재 컴포넌트가 위임하도록 한다.

--- src/app/(AfterLogin)/(interview)/interview/setting/_pages/QuestionPick/_component/SelectQuestionSection.tsx
+++ src/app/(AfterLogin)/(interview)/interview/setting/_pages/QuestionPick/_component/SelectQuestionSection.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from "react";
 import { useInterviewOption } from "../../../../../_lib/contexts/InterviewOptionContext";
 import { useGetQuestions } from "../../../_lib/queries/useQuestions";
 import QuestionList from "./QuestionList";
@@ -10,10 +9,35 @@ interface QuestionSelectionSectionProps {
 }
 
 // TODO: 컴포넌트 분리 -> SelectForm 등의 공용 컴포넌트로 분리하면 좋을 것 같다.
+// 또는 훅으로 분리해도 좋을듯
 const QuestionSelectionSection = ({ questionId, questionTitle }: QuestionSelectionSectionProps) => {
   const { setInterviewOption, interviewOption } = useInterviewOption();
   const { data: questionList, isLoading } = useGetQuestions({ interviewId: questionId });
+
   const getPrevQuestionIds = () => interviewOption.questions.map((question) => question.questionId);
+  const selectAllQuestions = () => {
+    if (!questionList) return;
+    setInterviewOption((prev) => {
+      const prevQuestionIds = interviewOption.questions.map((question) => question.questionId);
+      if (questionList.every((question) => prevQuestionIds.includes(question.questionId))) {
+        return {
+          ...prev,
+          questions: prev.questions.filter(
+            (prevQuestion) =>
+              !questionList
+                .map((question) => question.questionId)
+                .includes(prevQuestion.questionId),
+          ),
+        };
+      }
+
+      return {
+        ...prev,
+        questions: [...prev.questions, ...questionList],
+      };
+    });
+  };
+
   const selectQuestion = (id: number) => {
     if (!questionList) return;
     setInterviewOption((prev) => {
@@ -33,18 +57,22 @@ const QuestionSelectionSection = ({ questionId, questionTitle }: QuestionSelecti
     });
   };
 
-  // TODO: 로딩 컴포넌트
+  // TODO: 로딩 컴포넌트 구현
   if (isLoading) return <div>Loading...</div>;
 
   // TODO: header부분을 QuestionList와 추상화 레벨 맞추면 좋을 것 같다.
   return (
     <div className="flex-col w-full ml-2">
       <div className="rounded-md border w-full h-96 overflow-y-auto">
-        <QuestionListHeader questionList={questionList} questionTitle={questionTitle} />
+        <QuestionListHeader
+          questionList={questionList}
+          questionTitle={questionTitle}
+          onSelect={selectAllQuestions}
+        />
         <QuestionList
           questionId={questionId}
-          onSelect={selectQuestion}
           interviewOption={interviewOption}
+          onSelect={selectQuestion}
         />
       </div>
     </div>
