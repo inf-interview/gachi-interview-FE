c32c88583aa17f0c861b3f66f9c27ed9a07e3e42
Lee-Young-Jae <ori_@kakao.com>
2024-04-30 15:58:10 +0900

    fix: 프로젝트 위치를 변경한다.

---
+++ src/app/(AfterLogin)/(interview)/interview/record/_lib/hook/useTimer.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState, useRef } from "react";
+
+interface useTimerProps {
+  endSeconds: number;
+  onStop: () => void;
+  onTimeChange?: (time: number) => void;
+}
+
+const useTimer = ({ endSeconds, onStop, onTimeChange }: useTimerProps) => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const timer = useRef<NodeJS.Timeout | null>(null);
+
+  const clearTimer = () => {
+    if (timer.current) {
+      clearInterval(timer.current);
+      timer.current = null;
+    }
+  };
+
+  useEffect(() => {
+    if (isRunning) {
+      let innerTime = time;
+      timer.current = setInterval(() => {
+        innerTime++;
+        setTime(innerTime);
+        onTimeChange && onTimeChange(innerTime);
+        if (innerTime === endSeconds) {
+          clearTimer();
+          setIsRunning(false);
+          onStop();
+        }
+      }, 1000);
+    }
+
+    return clearTimer;
+  }, [isRunning]);
+
+  const start = () => {
+    if (isRunning) return;
+    setIsRunning(true);
+  };
+
+  const pause = () => {
+    setIsRunning(false);
+    clearTimer();
+  };
+
+  const reset = () => {
+    setIsRunning(false);
+    clearTimer();
+    setTime(0);
+  };
+
+  return { time, start, pause, reset };
+};
+
+export default useTimer;
