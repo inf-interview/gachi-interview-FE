dd9acf512ad4714d8f087d7ae68d64f4e7229438
Lee-Young-Jae <ori_@kakao.com>
2024-05-08 21:43:05 +0900

    refactor: 댓글을 별도의 컴포넌트로 분리한다.

--- src/app/(AfterLogin)/videos/[videoId]/_component/Comments.tsx
+++ src/app/(AfterLogin)/videos/[videoId]/_component/Comments.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { formatRelativeTime } from "@/lib/utills/days";
 import { useGetCommentsQuery } from "../../_lib/queries/useCommentQuery";
 import CreateComment from "./CreateComment";
+import Comment from "./Comment";
 
 interface CommentsProps {
   videoId: string;
@@ -15,26 +15,13 @@ const Comments = ({ videoId }: CommentsProps) => {
   if (isLoading) return <div>Loading...</div>;
   if (!comments) return <CreateComment videoId={videoId} />;
 
+  // TODO: userId 수정
   return (
-    <div className="flex flex-col mt-8">
+    <div className="flex flex-col mt-8 pb-8">
       <h2 className="text-2xl font-bold">피드백 {comments.length || ""}</h2>
       <CreateComment videoId={videoId} />
       {comments.map((comment, index) => (
-        <div key={index} className="flex flex-col mt-4">
-          <div className="flex items-center">
-            <img
-              className="w-10 h-10 rounded-full object-cover"
-              // TODO: 이미지 경로 수정
-              src="https://file.mk.co.kr/meet/neds/2020/03/image_readtop_2020_256728_15839167074119784.jpg"
-              alt="프로필 이미지"
-            />
-            <div className="flex flex-col ml-4">
-              <span className="font-bold">{comment.userName}</span>
-              <span className="text-gray-500">{formatRelativeTime(comment.createdAt)}</span>
-            </div>
-          </div>
-          <p className="mt-4">{comment.content}</p>
-        </div>
+        <Comment key={index} videoId={videoId} comment={comment} />
       ))}
     </div>
   );
