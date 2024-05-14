90293756291281d4a581633a44b189bc927dee85
dltmdgkr <shl907@naver.com>
2024-05-14 14:37:03 +0900

    refactor: kakao login

--- src/app/(BeforeLogin)/_component/Login.tsx
+++ src/app/(BeforeLogin)/_component/Login.tsx
@@ -4,25 +4,12 @@ import { Button } from "@/components/ui/button";
 import interviewImage from "../../../../public/interview.json";
 import dynamic from "next/dynamic";
 import { RiKakaoTalkFill } from "react-icons/ri";
-import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { KAKAO_AUTH_URL } from "../_lib/kakao";
 
 const Lottie = dynamic(() => import("react-lottie-player"), { ssr: false });
 
 export default function Login() {
-  const router = useRouter();
-  const onClick = async () => {
-    try {
-      console.log("로그인 전");
-      await signIn("kakao");
-      console.log("로그인 성공");
-      router.replace("/my?tab=videos");
-      console.log("이동 전");
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
   return (
     <div className="flex justify-center items-center">
       <div className="flex-col">
@@ -30,13 +17,12 @@ export default function Login() {
           같이 면접 준비하는 서비스 <br />
           같이 면접 <br />
         </p>
-        <Button
-          className="w-[350px] h-[70px] rounded-full bg-[#FEE500] text-black text-xl"
-          onClick={onClick}
-        >
-          <RiKakaoTalkFill className="mr-2" />
-          Kakao로 시작하기
-        </Button>
+        <Link href={KAKAO_AUTH_URL}>
+          <Button className="w-[350px] h-[70px] rounded-full bg-[#FEE500] text-black text-xl">
+            <RiKakaoTalkFill className="mr-2" />
+            Kakao로 시작하기
+          </Button>
+        </Link>
       </div>
       <Lottie play animationData={interviewImage} className="m-20 w-1/3 h-1/3" />
     </div>
