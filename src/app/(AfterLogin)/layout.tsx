dd71b326584d37d17a816080938d9a500ef97fbe
dltmdgkr <lseunghak1301@gmail.com>
2024-05-26 03:30:04 +0900

    feat: 게시글 작성 중 뒤로가기 버튼 클릭 시 모달창 띄우기

--- src/app/(AfterLogin)/layout.tsx
+++ src/app/(AfterLogin)/layout.tsx
@@ -9,10 +9,14 @@ import { HiOutlineBellAlert } from "react-icons/hi2";
 import { usePathname, useRouter } from "next/navigation";
 import RQProvider from "./_component/RQProvider";
 import AlertMessage from "./_component/AlertMessage";
+import { useModal } from "@/components/Modal/useModal";
+import Modal from "@/components/Modal";
+import { Button } from "@/components/ui/button";
 
 export default function AfterLoginLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
   const router = useRouter();
+  const { openModal, closeModal } = useModal();
   const navItems = [
     { href: "/interview/setting", icon: SlCamrecorder, label: "영상 녹화" },
     { href: "/videos", icon: MdOutlinePlaylistPlay, label: "면접 영상 목록" },
@@ -20,6 +24,37 @@ export default function AfterLoginLayout({ children }: { children: ReactNode }) 
     { href: "/my?tab=videos", icon: CgProfile, label: "마이페이지" },
     { href: "/alerts", icon: HiOutlineBellAlert, label: "알림" },
   ];
+
+  const handleBackClick = () => {
+    if (pathname.startsWith("/community/create")) {
+      openModal(
+        <Modal
+          header="글 작성을 종료하시겠습니까?"
+          disableBackdropClick={false}
+          footer={
+            <div className="flex justify-end gap-2">
+              <Button onClick={closeModal} variant="outline">
+                계속하기
+              </Button>
+              <Button
+                onClick={() => {
+                  router.back();
+                  closeModal();
+                }}
+                variant="destructive"
+              >
+                종료하기
+              </Button>
+            </div>
+          }
+        >
+          <p>작성 중이신 내용은 삭제됩니다.</p>
+        </Modal>,
+      );
+    } else {
+      router.back();
+    }
+  };
 
   return (
     <div className="flex">
@@ -64,9 +99,7 @@ export default function AfterLoginLayout({ children }: { children: ReactNode }) 
         width="30"
         xmlns="http://www.w3.org/2000/svg"
         className="text-gray-600 mt-5 ml-5"
-        onClick={() => {
-          router.back();
-        }}
+        onClick={handleBackClick}
       >
         <path d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"></path>
       </svg>
