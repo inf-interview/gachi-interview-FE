59e429bce9dcd62f86453d9720ae5f94701e946d
Lee-Young-Jae <ori_@kakao.com>
2024-06-05 23:23:01 +0900

    feat: 개별 질문을 삭제하는 기능을 추가한다.

--- src/app/(AfterLogin)/(interview)/interview/setting/_pages/QuestionPick/_component/QuestionItem.tsx
+++ src/app/(AfterLogin)/(interview)/interview/setting/_pages/QuestionPick/_component/QuestionItem.tsx
@@ -1,6 +1,10 @@
 import Modal from "@/components/Modal";
 import { useModal } from "@/components/Modal/useModal";
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineSearch, AiOutlineDelete } from "react-icons/ai";
+import { useDeleteQuestionMutation } from "../../../_lib/queries/useQuestions";
+import { useRecoilValue } from "recoil";
+import { userIdState } from "@/store/auth";
+import { Button } from "@/components/ui/button";
 
 interface QuestionItemProps {
   id: number;
@@ -8,19 +12,67 @@ interface QuestionItemProps {
   answer: string;
   checked: boolean;
   onSelect: (id: number) => void;
+  workbookId: number;
 }
 
-const QuestionItem = ({ id, content, answer, onSelect, checked }: QuestionItemProps) => {
-  const { openModal } = useModal();
+const QuestionItem = ({
+  id,
+  content,
+  answer,
+  onSelect,
+  checked,
+  workbookId,
+}: QuestionItemProps) => {
+  const { openModal, closeModal } = useModal();
+  const { mutate, isSuccess } = useDeleteQuestionMutation();
+  const userId = useRecoilValue(userIdState);
 
-  const handleSearch = () => {
+  const openDeleteModal = () => {
+    const handleDelete = () => {
+      mutate({
+        userId,
+        questionId: id,
+        workbookId: workbookId,
+      });
+
+      closeModal();
+    };
+
+    openModal(
+      <Modal
+        header="질문 삭제"
+        footer={
+          <>
+            <Button variant="secondary" onClick={closeModal}>
+              취소
+            </Button>
+            <Button variant="destructive" onClick={handleDelete}>
+              삭제
+            </Button>
+          </>
+        }
+      >
+        <p>선택한 질문을 삭제하시겠습니까?</p>
+      </Modal>,
+    );
+  };
+
+  const openDetailModal = () => {
     openModal(
       <Modal header="질문 상세" footer={null}>
-        <div className="flex flex-col">
+        <div className="flex flex-col w-full">
           <label className="text-gray-500">Q.</label>
-          <p className="text-gray-700">{content}</p>
+          <textarea
+            className="text-gray-700 text-sm bg-gray-100 p-2 rounded-md break-words resize-none focu"
+            value={content}
+            readOnly
+          />
           <label className="text-gray-500 mt-4">A.</label>
-          <p className="text-gray-700">{answer}</p>
+          <textarea
+            className="w-full text-gray-700 text-sm bg-gray-100 p-2 rounded-md break-words resize-none h-36"
+            value={answer}
+            readOnly
+          />
         </div>
       </Modal>,
     );
@@ -36,13 +88,23 @@ const QuestionItem = ({ id, content, answer, onSelect, checked }: QuestionItemPr
         onChange={() => onSelect(id)}
         value={id}
       />
-      <label htmlFor={id.toString()} className="ml-12 cursor-pointer">
+
+      <label htmlFor={id.toString()} className="ml-12 cursor-pointer text-gray-700 line-clamp-1">
         {content}
       </label>
-      <AiOutlineSearch
-        onClick={handleSearch}
-        className="ml-auto cursor-pointer opacity-0 group-hover:opacity-100"
-      />
+      <div
+        className="ml-auto flex items-center space-x-2 opacity-0 group-hover:opacity-100 transition-opacity"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <AiOutlineDelete
+          onClick={openDeleteModal}
+          className="cursor-pointer opacity-0 group-hover:opacity-100 hover:scale-110 transition-transform duration-200"
+        />
+        <AiOutlineSearch
+          onClick={openDetailModal}
+          className="cursor-pointer opacity-0 group-hover:opacity-100 hover:scale-110 transition-transform duration-200"
+        />
+      </div>
     </li>
   );
 };
