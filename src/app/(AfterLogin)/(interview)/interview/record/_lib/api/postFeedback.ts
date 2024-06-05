c2b22fddfaac82e5cc04726e0e7e88bb8a8c3c70
Lee-Young-Jae <ori_@kakao.com>
2024-06-05 20:13:43 +0900

    feat: feedback 트리거를 요청하는 로직을 추가한다.

---
+++ src/app/(AfterLogin)/(interview)/interview/record/_lib/api/postFeedback.ts
@@ -0,0 +1,22 @@
+import customFetcher from "@/utils/customFetcher";
+
+export interface PostFeedbackRequest {
+  videoId: number;
+  content: string;
+}
+
+const postFeedback = async ({ videoId, content }: PostFeedbackRequest) => {
+  const { response, data } = await customFetcher(`/feedback/${videoId}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      content: content,
+    }),
+  });
+
+  return data;
+};
+
+export default postFeedback;
