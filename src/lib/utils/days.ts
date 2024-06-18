6d18fb11c2a955f154dd06d2805efb654cc21c62
Lee-Young-Jae <ori_@kakao.com>
2024-06-18 15:58:47 +0900

    feat: 3일이 넘어가면 년. 월. 일. 포맷으로 출력되도록 기능추가

--- src/lib/utils/days.ts
+++ src/lib/utils/days.ts
@@ -2,6 +2,9 @@
  * "2024-01-01 15:30:00" 형식의 시간을 "5분 전", "1시간 전", "1일 전" 과 같은 상대적인 시간으로 변환합니다.
  */
 export const formatRelativeTime = (time: string) => {
+  // 3일이 지나면 2024. 1. 1. 형식으로 표시
+  const MAX_DIFF = 3 * 24 * 3600 * 1000;
+
   const currentTime = new Date();
   const targetTime = new Date(time);
   const diff = currentTime.getTime() - targetTime.getTime();
@@ -11,9 +14,12 @@ export const formatRelativeTime = (time: string) => {
     return "방금 전";
   } else if (diffMinutes < 60) {
     return `${diffMinutes}분 전`;
-  } else if (diffHours < 24) {
+  }
+  if (diffHours < 24) {
     return `${diffHours}시간 전`;
-  } else {
+  }
+  if (diff < MAX_DIFF) {
     return `${Math.floor(diffHours / 24)}일 전`;
   }
+  return `${targetTime.getFullYear()}. ${targetTime.getMonth() + 1}. ${targetTime.getDate()}.`;
 };
