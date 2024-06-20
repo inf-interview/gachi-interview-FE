d0679f17203c653a928f97c2c68816b579425106
Lee-Young-Jae <ori_@kakao.com>
2024-06-20 18:07:04 +0900

    feat: 사용자 음성을 텍스트로 확인할 수 있도록 한다.

--- src/app/(AfterLogin)/(interview)/interview/record/_component/Viewer.tsx
+++ src/app/(AfterLogin)/(interview)/interview/record/_component/Viewer.tsx
@@ -1,15 +1,29 @@
-import { QuestionType } from "../../../_lib/contexts/InterviewOptionContext";
+import { ReactNode } from "react";
+import { QuestionType } from "../../../_lib/atoms/interviewState";
 
 interface QuestionListViewerProps {
   questionId: number;
   questionList: QuestionType[];
+  timer: ReactNode;
 }
 
-export const QuestionViewer = ({ questionId, questionList }: QuestionListViewerProps) => {
+export const QuestionViewer = ({ questionId, questionList, timer }: QuestionListViewerProps) => {
+  const questionContent = questionList.find(
+    (question) => question.questionId === questionId,
+  )?.questionContent;
+  const questionNumber =
+    questionList.findIndex((question) => question.questionId === questionId) + 1;
+
   return (
-    <p className="absolute top-6 m-4 right-0 bg-black bg-opacity-50 p-4 rounded-lg text-white text-sm z-10 scale-x-[-1]">
-      Q. {questionList.find((question) => question.questionId === questionId)?.questionContent}
-    </p>
+    <div className="fixed top-0 left-0 right-0 w-[80%] bg-black bg-opacity-70 p-4 rounded-b-lg text-white text-base z-10 m-auto">
+      <div className="flex items-center w-full justify-between">
+        <span className="bg-white text-black px-4 py-1 rounded-2xl text-base font-bold min-w-[50px] text-center">
+          {questionNumber}번 질문
+        </span>
+        <span className="py-2 text-lg flex-1 text-center mx-2">{questionContent}</span>
+        {timer}
+      </div>
+    </div>
   );
 };
 
@@ -19,7 +33,7 @@ interface AnswerViewerProps {
 }
 export const AnswerViewer = ({ questionId, questionList }: AnswerViewerProps) => {
   return (
-    <p className="absolute bottom-0 m-4 right-0 bg-black bg-opacity-50 p-4 rounded-lg text-white text-xs z-10 scale-x-[-1] overflow-auto">
+    <p className="absolute bottom-0 m-4 right-0 bg-black bg-opacity-50 p-4 rounded-lg text-white text-xs z-10 overflow-auto">
       A. {questionList.find((question) => question.questionId === questionId)?.answerContent}
     </p>
   );
