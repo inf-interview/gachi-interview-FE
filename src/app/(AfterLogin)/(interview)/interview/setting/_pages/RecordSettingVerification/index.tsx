630c18cafae73adf2fea8411b2030ccdb32ad736
Lee-Young-Jae <ori_@kakao.com>
2024-06-20 20:26:46 +0900

    style: 유저 인터페이스 변경

--- src/app/(AfterLogin)/(interview)/interview/setting/_pages/RecordSettingVerification/index.tsx
+++ src/app/(AfterLogin)/(interview)/interview/setting/_pages/RecordSettingVerification/index.tsx
@@ -6,7 +6,7 @@ import {
   mediaOptionState,
 } from "@/app/(AfterLogin)/(interview)/_lib/atoms/interviewState";
 import { useRecoilValue } from "recoil";
-import { Suspense, useEffect } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useErrorModal } from "@/components/Modal/useModal";
 import Ready from "./_component/Ready";
 
@@ -23,15 +23,22 @@ const RecordSettingVerification = ({ setStep }: RecordSettingVerificationProps) 
   const messages = [
     { message: "면접장을 정리하고 있어요.", icon: "🧹" },
     { message: "AI를 잠에서 깨우고 있어요.", icon: "🤖" },
-    { message: "면접 AI에 충전기 꼽는 중...", icon: "🔌" },
+    { message: "면접 AI를 충전시키는 중...", icon: "🔌" },
     { message: "인코딩 서버를 부팅 중...", icon: "🚀" },
     { message: "곧 면접을 시작할게요!", icon: "🎥" },
     { message: "그럴싸해보이는 답변을 생각 중...", icon: "💡" },
-    { message: "녹음이 잘 되도록 준비 중...", icon: "🎙️" },
-    { message: "면접 AI가 면접관을 찾는 중...", icon: "🔍" },
+    { message: "녹음이 잘 되도록 스탠딩 마이크를 설치 중...", icon: "🎤" },
+    { message: "AI를 면접관으로 학습중...", icon: "🔍" },
+    // { message: "그거 아시나요? 면접 AI는 무한한 인내심을 가지고 있어요.", icon: "🤫" },
+    { message: "질문을 상세하게 분석 중...", icon: "🔍" },
+    { message: "그거 아시나요? 질문을 상세히 작성할수록 면접 AI가 더 똑똑해져요.", icon: "🧠" },
+    { message: "저희 서비스는 면접 퀄리티를 위해 녹화시간을 5분으로 제한하고 있어요.", icon: "⏱" },
   ];
 
-  const randomIndex = Math.floor(Math.random() * messages.length);
+  const [currentMessageIndex, setCurrentMessageIndex] = useState(
+    Math.floor(Math.random() * messages.length),
+  );
+  const [fadeIn, setFadeIn] = useState(true);
 
   const validateInterviewOption = () => {
     if (!interviewOption) {
@@ -55,27 +62,45 @@ const RecordSettingVerification = ({ setStep }: RecordSettingVerificationProps) 
   };
 
   useEffect(() => {
-    const LOADING_TIME = 2500;
+    const LOADING_TIME = 4000;
 
     if (!validateInterviewOption()) {
       return;
     }
 
+    const changeMessage = () => {
+      setFadeIn(false);
+      const nextMessageIndex = (currentMessageIndex + 1) % messages.length;
+      setTimeout(() => {
+        setCurrentMessageIndex(nextMessageIndex);
+        setFadeIn(true);
+      }, 1000);
+    };
+
     const timer = setTimeout(() => {
       router.push("/interview/record");
     }, LOADING_TIME);
 
+    const messageTimer = setInterval(changeMessage, LOADING_TIME / 2); // 횟수 조절
+
     return () => {
       clearTimeout(timer);
+      clearInterval(messageTimer);
     };
   }, []);
 
   return (
-    <div className="flex flex-col items-center w-full h-full justify-center p-8">
+    <div className="flex flex-col items-center w-full h-[80%] justify-center p-8">
       <Suspense fallback={<span className="loader" />}>
         <Ready />
       </Suspense>
-      <div className="text-xl font-semibold message h-16">{messages[randomIndex].message}</div>
+      <div
+        className={`text-xl font-semibold message h-16 ${
+          fadeIn ? "animate-fadeIn" : "animate-fadeOut"
+        }`}
+      >
+        {messages[currentMessageIndex].message}
+      </div>
     </div>
   );
 };
