ef7559cd0cec683a99c87c312374bc8f156730d3
Lee-Young-Jae <ori_@kakao.com>
2024-07-01 00:32:41 +0900

    feat: Add keywords for SEO optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,14 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.png",
   },
+  keywords: ["같이면접", "면접", "면접준비", "면접연습", "AI면접관"],
   applicationName: "같이면접",
   openGraph: {
     title: "같이면접",
-    description: "같이 면접 준비하는 서비스.",
+    description: "면접 준비 어려우셨죠? 같이 준비해요!. AI 면접관과 함께하는 면접 연습 서비스",
     type: "website",
     url: "https://gachi-interview.vercel.app",
+    locale: "ko_KR",
     images: [
       {
         url: "/logo.png",
